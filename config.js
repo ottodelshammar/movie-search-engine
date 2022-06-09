0184dbfa333fce9432bd3c815d635d3aac80941d
ottodelshammar <ottodelshammar@gmail.com>
2022-06-09 17:08:55 +0200

    initial commit

---
+++ config.js
@@ -0,0 +1,12 @@
+const config = {
+    db: {
+        host: "localhost",
+        user: "root",
+        password: "password",
+        database: "movie_db",
+        port: "3306",
+        multipleStatements: true
+    }
+};
+
+module.exports = {config};
---
+++ create-db.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql');
+const { config } = require('./api/config.js');
+
+var connection = mysql.createConnection(config.db);
+
+connection.connect((err) => {
+    if (err) {
+        throw err
+    } else {
+        console.log("connected")
+    }
+})
+
+connection.query('SET GLOBAL local_infile=1;' +
+    'DROP TABLE IF EXISTS movie_db.title;' +
+    'CREATE TABLE movie_db.title(tconst CHAR(10), titleType VARCHAR(255), primaryTitle VARCHAR(1000), originalTitle VARCHAR(1000), isAdult VARCHAR(255), startYear VARCHAR(255), endYear VARCHAR(255), runtimeMinutes VARCHAR(255), genres VARCHAR(255), PRIMARY KEY (tconst));' +
+    'LOAD DATA LOCAL INFILE "C:/Users/otto.delshammar/movie-search-engine/data_title.basics.tsv" INTO TABLE title;', (err, rows) => {
+        if (err) {
+            throw err
+        } else {
+            console.log("DATA SENT BOIS")
+            console.log(rows)
+        }
+})
